a8957c316a42b643e373131d86001993f19e8e0e
Frederic Larocque <frederic.d.larocque@gmail.com>
2023-03-18 17:49:29 +0100

    Prevent pusher creating negative thrust

--- functions/Fx_pusher.m
+++ functions/Fx_pusher.m
@@ -6,7 +6,12 @@ function F_x = Fx_pusher(pusher_prop_RPM,V)
 % pusher_RPM2Fx_coeff = [3.703544293799073e-07 -6.005961822003111e-05 0]; %without offset
 Fx_pusher_coeff = [3.680278471152158e-07 -4.391797235636297e-05 -8.651770063289748E-2];
 
-F_x = Fx_pusher_coeff(1) .* pusher_prop_RPM.^2   +...
-      Fx_pusher_coeff(2) .* pusher_prop_RPM .* V +...
-      Fx_pusher_coeff(3) .* V;
+if V>0
+    F_x = Fx_pusher_coeff(1) .* pusher_prop_RPM.^2   +...
+          Fx_pusher_coeff(2) .* pusher_prop_RPM .* V +...
+          Fx_pusher_coeff(3) .* V;
+else
+   F_x = Fx_pusher_coeff(1) .* pusher_prop_RPM.^2;
+end
+
 end
