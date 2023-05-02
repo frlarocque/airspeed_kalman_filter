9af85be1c7df40eb712f320d99cbae32829c72a0
Frederic Larocque <frederic.d.larocque@gmail.com>
2023-05-02 10:57:50 +0200

    Partial checkout of files from filt cov branch

---
+++ kalman_filter_based/kalman_filter_settings.m
@@ -0,0 +1,81 @@
+%% Kalman Filter
+% x = [u v w mu_x mu_y mu_z k_x k_y k_z];
+% 𝑢, 𝑣, 𝑤 = Velocity in the Body Frame
+% 𝜇𝑥, 𝜇𝑦, 𝜇𝑧 = Wind Velocity in Earth Fixed Frame
+% k_x, k_y, k_z = Offset states for identification of parameters
+
+% u = [a_x a_y a_z p q r phi theta psi RPM_pusher RPM_hover skew elevator_pprz];
+% 𝑎x, 𝑎𝑦, 𝑎𝑧 = Accelerations Measured by Accelerometers
+% 𝑝, 𝑞, 𝑟 = Angular Rates Measured by Gyroscopes
+% phi, theta, psi = euler angles
+% RPM_pusher = pusher rpm
+% RPM_hover = mean RPM of hover propellers
+% skew = skew angle of wing
+% elevator pprz = elevator position
+
+% z = [Vx Vy Vz a_x_filt a_y_filt a_z_filt V_pitot]
+% 𝑉𝑥 , 𝑉𝑦, 𝑉𝑧 = Velocity in Earth Fixed Frame
+% a_x_filt a_y_filt a_z_filt = body accelerations filtered
+% V_pitot = airspeed measured by pitot
+
+global EKF_AW_AZ_QUICK_CONV_ACCEL_GAIN EKF_AW_AZ_QUICK_CONV_MU_GAIN EFK_AW_QUICK_CONVERGENCE EKF_AW_QUICK_CONVERGENCE_TIME
+global EKF_AW_USE_MODEL_BASED EKF_AW_USE_BETA EKF_AW_WING_INSTALLED EKF_AW_PROPAGATE_OFFSET EKF_AW_VEHICLE_MASS EKF_AW_USE_PITOT 
+global EKF_AW_AZ_SCHED_GAIN EKF_AW_AZ_SCHED_START_DEG EKF_AW_AZ_SCHED_END_DEG
+global EKF_AW_AX_SCHED_GAIN EKF_AW_AX_SCHED_START_DEG EKF_AW_AX_SCHED_END_DEG
+global EKF_AW_Q_accel EKF_AW_Q_gyro EKF_AW_Q_mu EKF_AW_Q_offset
+global EKF_AW_R_V_gnd EKF_AW_R_accel_filt_x EKF_AW_R_accel_filt_y EKF_AW_R_accel_filt_z EKF_AW_R_V_pitot
+global EKF_AW_P0_V_body EKF_AW_P0_mu EKF_AW_P0_offset
+global filter_low_freq filter_high_freq f_EKF
+
+EKF_AW_USE_MODEL_BASED = true;
+EKF_AW_USE_BETA = false;
+EKF_AW_WING_INSTALLED = true;
+EKF_AW_PROPAGATE_OFFSET = false;
+EKF_AW_USE_PITOT = false;
+
+EFK_AW_QUICK_CONVERGENCE = true;
+EKF_AW_QUICK_CONVERGENCE_TIME = 20;
+EKF_AW_AZ_QUICK_CONV_MU_GAIN = 2;
+EKF_AW_AZ_QUICK_CONV_ACCEL_GAIN = 0;
+
+EKF_AW_AZ_SCHED_GAIN = 2;
+EKF_AW_AZ_SCHED_START_DEG = 60;
+EKF_AW_AZ_SCHED_END_DEG = 70;
+EKF_AW_AX_SCHED_GAIN = 0;
+EKF_AW_AX_SCHED_START_DEG = 40;
+EKF_AW_AX_SCHED_END_DEG = 60;
+
+if EKF_AW_WING_INSTALLED
+    EKF_AW_VEHICLE_MASS = 4.2;
+else
+    EKF_AW_VEHICLE_MASS = 4.2;
+end
+
+
+
+EKF_AW_Q_accel = 1E-04;
+EKF_AW_Q_gyro = 1E-09;
+EKF_AW_Q_mu = 1E-4;
+EKF_AW_Q_offset = 1E-8;
+
+EKF_AW_R_V_gnd = 1E-05;
+EKF_AW_R_accel_filt_x = 1E-5;
+EKF_AW_R_accel_filt_y = 1E-5;
+EKF_AW_R_accel_filt_z = 1E-5;
+EKF_AW_R_V_pitot = 1E-8;
+
+EKF_AW_P0_V_body = 1E-2;
+EKF_AW_P0_mu = 1E1*EKF_AW_Q_mu;
+EKF_AW_P0_offset = EKF_AW_Q_offset;
+
+filter_low_freq = 0.2; %[Hz]
+filter_high_freq = 24; %[Hz]
+f_EKF = 5; %[Hz]
+
+f_fh = str2func('f_4');
+g_fh = str2func('g_14_cst');
+
+epsi = 1E-2;
+
+% Initial conditions
+x_0 = [0 0 0   0 0 0   0 0 0]';
--- kalman_filter_based/single_run_complete_filter.m
+++ kalman_filter_based/single_run_complete_filter.m
@@ -6,14 +6,13 @@ clc
 format long
 % Add all paths
 addpath('/home/frederic/Documents/thesis/tools/airspeed_estimation/functions/');
-
-% Load single filedata
-log_path = "/home/frederic/Documents/thesis/tools/airspeed_estimation/mat_files/log/wind_tunnel_prototype";
+%% Load single filedata
+clear ac_data
+log_path = '/home/frederic/Documents/thesis/tools/airspeed_estimation/mat_files/log';
 [file,path] = uigetfile({'*.mat'},'Select a file',log_path);
 
 load(fullfile(path,file))
-
-%% Setup
+%% Select relevant data
 % Setup Options
 graph = 0;
 beta_est = 1;
@@ -23,248 +22,109 @@ pitot_correction = 1.0;
 
 % Run setup
 wind_triangle_setup
-
-%% Kalman Filter Without Euler angle estimation
-% 𝑢, 𝑣, 𝑤 = Velocity in the Body Frame
-% 𝜇𝑥, 𝜇𝑦, 𝜇𝑧 = Wind Velocity in Earth Fixed Frame
-% 𝑝, 𝑞, 𝑟 = Angular Rates Measured by Gyroscopes
-% 𝑎x, 𝑎𝑦, 𝑎𝑧 = Accelerations Measured by Accelerometers
-% 𝑉𝑥 , 𝑉𝑦, 𝑉𝑧 = Velocity in Earth Fixed Frame
-% alpha, beta = angle of attack and sideslip angle
-
-% x = [u v w mu_x mu_y mu_z k_x k_y k_z];
-% u = [a_x a_y a_z p q r phi theta psi RPM_pusher RPM_hover skew elevator_pprz];
-% z = [V_x V_y V_z a_x a_y a_z pitot];
-
-%w_w = [0;0;0]; %noise angular rate
-%w_a = [0;0;0]; %noise accelerations
-%w_mu =[0;0;0]; %noise wind
-
-global EKF_AW_USE_MODEL_BASED EKF_AW_USE_BETA EKF_AW_WING_INSTALLED EKF_AW_PROPAGATE_OFFSET EKF_AW_VEHICLE_MASS EKF_AW_USE_PITOT
-
-EKF_AW_USE_MODEL_BASED = true;
-EKF_AW_USE_BETA = false;
-EKF_AW_WING_INSTALLED = true;
-EKF_AW_PROPAGATE_OFFSET = false;
-EKF_AW_USE_PITOT = true;
-
-if EKF_AW_WING_INSTALLED
-    EKF_AW_VEHICLE_MASS = 4.2;
-else
-    EKF_AW_VEHICLE_MASS = 4.2;
-end
-
-EKF_AW_Q_accel = 1E-04;
-EKF_AW_Q_gyro = 1E-09;
-EKF_AW_Q_mu = 1E-4;
-EKF_AW_Q_offset = 1E-8;
-
-EKF_AW_R_V_gnd = 1E-05;
-EKF_AW_R_accel_filt_x = 1E-5;
-EKF_AW_R_accel_filt_y = 1E-5;
-EKF_AW_R_accel_filt_z = 1E-5;
-EKF_AW_R_V_pitot = 1E-8;
-
-EKF_AW_P0_V_body = 1E-2;
-EKF_AW_P0_mu = 1E1*EKF_AW_Q_mu;
-EKF_AW_P0_offset = EKF_AW_Q_offset;
-
-
-epsi = 1E-2;
-
+%% Kalman Filter Settings
+kalman_filter_settings
+%% Filtering and resampling
 t = airspeed_pitot.flight.time;
 dt = mean(diff(t));
 
-f_fh = str2func('f_4');
-g_fh = str2func('g_14_cst');
-
 % Get filter accel agressively
-filter_freq = 0.2; %[Hz]
-[b,a] = butter(2,2*filter_freq*dt,'low');
+[b,a] = butter(2,2*filter_low_freq*dt,'low');
 
 a_x_filt = filter(b,a,IMU_accel.flight.data(:,1));
 a_y_filt = filter(b,a,IMU_accel.flight.data(:,2));
 a_z_filt = filter(b,a,IMU_accel.flight.data(:,3));
-pusher_prop_pwm_filt = filtfilt(b,a,pusher_prop_pwm.flight.data);%filter(b,a,pusher_prop_rpm.flight.data);
-hover_prop_pwm_filt = filtfilt(b,a,mean(hover_prop_pwm.flight.data,2));%filter(b,a,mean(hover_prop_rpm.flight.data,2));
+pusher_prop_rpm_filt = filter(b,a,pusher_prop_rpm.flight.data);%filter(b,a,pusher_prop_rpm.flight.data);
+hover_prop_rpm_filt = filter(b,a,mean(hover_prop_rpm.flight.data,2));%filter(b,a,mean(hover_prop_rpm.flight.data,2));
 skew_filt = filter(b,a,skew.flight.data);
-elevator_pwm_filt = filter(b,a,control_surface_pwm.flight.data(:,4));
-
-% Initial conditions
-x_0 = [0 0 0   0 0 0   0 0 0]';
+elevator_pprz_filt = filter(b,a,control_surface_pprz.flight.data(:,4));
 
 u_list = [IMU_accel.flight.data IMU_rate.flight.data IMU_angle.flight.data ...
-            pusher_prop_pwm_filt hover_prop_pwm_filt skew_filt elevator_pwm_filt]';
+            pusher_prop_rpm_filt hover_prop_rpm_filt skew_filt elevator_pprz_filt]';
 z_list = [Vg_NED.flight.data a_x_filt a_y_filt a_z_filt airspeed_pitot.flight.data]'; %measurement
 
 % Filter Data coming in
-filter_freq = 10.0; %[Hz]
-[b,a] = butter(2,2*filter_freq*dt,'low');
+[b,a] = butter(2,2*filter_high_freq*dt,'low');
 u_list(1:6,:) = filtfilt(b,a,u_list(1:6,:)')';%filter(b,a,u_list(1:6,:),[],2);
 z_list(1:3,:) = filtfilt(b,a,z_list(1:3,:)')';%filter(b,a,z_list(1:3,:),[],2);
 
-kalman_res = {};
-
-Q = diag([[1 1 1].*EKF_AW_Q_accel,[1 1 1].*EKF_AW_Q_gyro,[1 1 1E-5].*EKF_AW_Q_mu,[1 1 1].*EKF_AW_Q_offset]); %process noise
+Q = diag([[1 1 1E1].*EKF_AW_Q_accel,[1 1 1].*EKF_AW_Q_gyro,[1 1 1E-2].*EKF_AW_Q_mu,[1 1 1].*EKF_AW_Q_offset]); %process noise
 P_0 = diag([[1 1 1].*EKF_AW_P0_V_body [1 1 1].*EKF_AW_P0_mu [1 1 1].*EKF_AW_P0_offset]); %covariance
-R = diag([[1 1 1E-3].*EKF_AW_R_V_gnd EKF_AW_R_accel_filt_x EKF_AW_R_accel_filt_y EKF_AW_R_accel_filt_z EKF_AW_R_V_pitot]); %measurement noise
+R = diag([[1 1 1].*EKF_AW_R_V_gnd EKF_AW_R_accel_filt_x EKF_AW_R_accel_filt_y EKF_AW_R_accel_filt_z EKF_AW_R_V_pitot]); %measurement noise
 
-%% Resample to different sample time
-f_EKF = 5; %Hz
-
+f_EKF = 5;
+% Resample to different sample time
 u_list = resample(u_list',t,f_EKF)';
 z_list = resample(z_list',t,f_EKF)';
 t = [t(1):1/f_EKF:t(end)]';
-
+dt = 1/f_EKF;
 airspeed = resample(airspeed_pitot.flight.data,airspeed_pitot.flight.time,f_EKF);
 
-%%
-fprintf('Duration %2.2f s\n',t(end)-t(1))
-EKF_res = {};
-
-x_list = zeros(size(x_0,1),length(t));
-y_list = zeros(size(z_list,1),length(z_list));
-
-K = cell(1,length(t));
-P = cell(1,length(t));
-S = cell(1,length(t));
-R_variable = cell(1,length(t));
-Q_variable = cell(1,length(t));
-
-h = waitbar(0,'Please wait...');
-for k=1:length(t)
-    
-    waitbar(k / length(t))
-
-    u=u_list(:,k);
-    z=z_list(:,k);
-
-    if k==1
-        x = x_0;
-        P_last = P_0;
-    else
-        x=x_list(:,k-1);
-        P_last = P{k-1};
-    end
-
-    Q_variable{k} = Q;
-    R_variable{k} = R;
-
-    
-    if t(k)-t(1)<0
-        if z(4)<0
-        Q_variable{k}(7,7) = 1E2*Q(7,7); %increase wind covariance --> it can change faster
-        Q_variable{k}(8,8) = 1E2*Q(8,8);
-        Q_variable{k}(9,9) = 1E2*Q(9,9);
-
-
-        R_variable{k}(4,4) = 1E-2*R(4,4); %decrease a_x cov --> more weight put on it
-        R_variable{k}(5,5) = 1E-2*R(5,5); %decrease a_x cov --> more weight put on it
-        end
-    end
-
-    % Don't use A_z if skew smaller than 60 deg
-    skew_crit = deg2rad(60);
-    skew_crit_band = deg2rad(20);
-    skew_crit_gain = 1;
-    if u(12) < skew_crit-skew_crit_band
-        R_variable{k}(6,6) = 10.^skew_crit_gain.*R(6,6);
-    elseif u(12) < skew_crit
-        R_variable{k}(6,6) = 10.^((-skew_crit_gain./(skew_crit_band)).*(u(12)-skew_crit)).*R(6,6);
-    else
-        R_variable{k}(6,6) = R(6,6);
-    end
-
-    F_val = F(f_fh,x,u,epsi);
-    G_val = G(g_fh,x,u,epsi);
-    L_val = L(f_fh,x,u,epsi);
-    M_val = M(g_fh,x,u,epsi);
-
-    % Prediction
-    x_pred = x + dt*f_fh(x,u);
-    P_pred = F_val*P_last*F_val'+L_val*Q_variable{k}*L_val';
-
-    % Innovation
-    y_list(:,k) = z-g_fh(x,u);
-
-    S{k} = G_val*P_pred*G_val'+M_val*R_variable{k}*M_val';
-
-    %Kalman gain
-    K{k} = P_pred*G_val'*inv(S{k});
-
-    % State update
-    x_list(:,k) = x_pred;
-
-    % V_gnd contribution
-    x_list(1:3,k) = x_list(1:3,k)+ K{k}(1:3,1:3)*y_list(1:3,k); % Update V_body
-    x_list(4:6,k) = x_list(4:6,k)+ K{k}(4:6,1:3)*y_list(1:3,k); % Update mu
-    if EKF_AW_PROPAGATE_OFFSET
-        x_list(7:9,k) = x_list(7:9,k)+ K{k}(7:9,1:3)*y_list(1:3,k); % Update offset
-    end
-    % A_filt contribution
-    x_list(1:3,k) = x_list(1:3,k)+ K{k}(1:3,4:6)*y_list(4:6,k); % Update V_body
-    x_list(4:6,k) = x_list(4:6,k)+ K{k}(4:6,4:6)*y_list(4:6,k); % Update mu
-    if EKF_AW_PROPAGATE_OFFSET
-        x_list(7:9,k) = x_list(7:9,k)+ K{k}(7:9,4:6)*y_list(4:6,k); % Update offset
-    end
-
-    % V_pitot contribution
-    if EKF_AW_USE_PITOT
-        x_list(1:3,k) = x_list(1:3,k)+ K{k}(1:3,7)*y_list(7,k); % Update V_body
-        x_list(4:6,k) = x_list(4:6,k)+ K{k}(4:6,7)*y_list(7,k); % Update mu
-        if EKF_AW_PROPAGATE_OFFSET
-            x_list(7:9,k) = x_list(7:9,k)+ K{k}(7:9,7)*y_list(7,k); % Update offset
-        end
-    end
-
-    P{k} = (eye(length(x))-K{k}*G_val)*P_pred;
-
-end
-
-EKF_res.t = t;
-EKF_res.x = x_list;
-EKF_res.y = y_list;
-EKF_res.u = u_list;
-EKF_res.z = z_list;
-EKF_res.P = P;
-EKF_res.Q = Q_variable;
-EKF_res.R = R_variable;
-EKF_res.K = K;
-EKF_res.S = S;
+%% Run filter
+kalman_res = {};
+[EKF_res] = run_EKF(epsi,t,Q,R,P_0,x_0,u_list,z_list,f_fh,g_fh,true);
 
 kalman_res{1} = EKF_res;
 
 kalman_res{1}.error = error_quantification(kalman_res{1}.x(1,logical(interp1(airspeed_pitot.flight.time,double(airspeed_pitot.flight.valid),t,'nearest')))',airspeed(logical(interp1(airspeed_pitot.flight.time,double(airspeed_pitot.flight.valid),t,'nearest'))));
 
 fprintf("FINISHED!\n \nWAKE UP!\n")
-close(h)
+
 %% Plot
 select = 1;
 %plot_EKF_result(kalman_res{select},airspeed_pitot.flight,wind)
 plot_EKF_result_full(kalman_res{select},airspeed_pitot.flight,beta.flight,alpha.flight,wind)
 fprintf('Estimated wind (using Kalman Filter) is %0.2f m/s going %0.2f deg\n',mean(vecnorm(kalman_res{select}.x(4:6,:),2)),rad2deg(atan2(mean(kalman_res{select}.x(4,:)),mean(kalman_res{select}.x(5,:)))))
 
-%%
+if EKF_AW_PROPAGATE_OFFSET
 figure;
 subplot(3,1,1)
 plot(kalman_res{1}.t,kalman_res{1}.x(7,:)')
 xlabel('Time [s]')
 ylabel('offset_x')
-
 subplot(3,1,2)
 plot(kalman_res{1}.t,kalman_res{1}.x(8,:)')
 xlabel('Time [s]')
 ylabel('offset_y')
-
 subplot(3,1,3)
 plot(kalman_res{1}.t,kalman_res{1}.x(9,:)')
 xlabel('Time [s]')
 ylabel('offset_z')
+end
 
-t_cond = u_list(12,:)>deg2rad(80);
-mean(kalman_res{1}.x(7,t_cond))
-mean(kalman_res{1}.x(8,t_cond))
-mean(kalman_res{1}.x(9,t_cond))
+figure;
+filter_freq = 0.1; %[Hz]
+[b,a] = butter(2,2*filter_freq*dt,'low');
+
+subplot(4,1,1)
+plot(kalman_res{1}.t,filtfilt(b,a,kalman_res{1}.y(1:3,:)')')
+grid on
+xlabel('Time [s]')
+ylabel('Innovation')
+title('V_{gnd}')
+legend('N','E','D')
+
+subplot(4,1,2)
+plot(kalman_res{1}.t,filtfilt(b,a,kalman_res{1}.y(4:6,:)')')
+grid on
+xlabel('Time [s]')
+ylabel('Innovation')
+title('Accel')
+legend('x','y','z')
+
+subplot(4,1,3)
+plot(kalman_res{1}.t,filtfilt(b,a,kalman_res{1}.y(7,:)')')
+grid on
+xlabel('Time [s]')
+ylabel('Innovation')
+title('Pitot')
+
+subplot(4,1,4)
+yyaxis left
+plot(kalman_res{1}.t,rad2deg(kalman_res{1}.u(12,:)))
+xlabel('Time [s]')
+ylabel('Skew [deg]')
+
 
 %% Plot covariance
 
@@ -277,11 +137,9 @@ end
 
 figure
 ax1 = subplot(2,1,1);
-semilogy(kalman_res{select}.t,R_temp(:,[4:6]))
-legend('A_x','A_y','A_z')
+semilogy(kalman_res{select}.t,R_temp(:,[4]))
 ax2 = subplot(2,1,2);
 semilogy(kalman_res{select}.t,Q_temp(:,[7:9]))
-legend('Offset')
 
 figure
 ax1 = subplot(2,1,1);
@@ -301,7 +159,7 @@ legend('mu_x','mu_y')
 grid on
 
 linkaxes([ax1,ax2],'x')
-%sgtitle(sprintf('Wind Covariance %.1d | RMS error %.2f',cov_list(select),kalman_res{select}.error.error_RMS))
+sgtitle(sprintf('Wind Covariance %.1d | RMS error %.2f',cov_list(select),kalman_res{select}.error.error_RMS))
 
 %% Plot gains
 figure
