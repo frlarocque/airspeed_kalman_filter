cf12efdf2f65031578b75fa5d6d4536e190a5331
lampadaire45 <frederic.d.larocque@gmail.com>
2023-05-19 14:25:44 +0200

    Initial residual fault detector

---
+++ kalman_filter_based/residual_fault_detector.m
@@ -0,0 +1,105 @@
+classdef residual_fault_detector < handle
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+
+    properties
+        res = 0; %[m/s]
+        res_filt = 0; %[m/s]
+        res_diff = 0; %[m/s]
+        
+        crit_low  = 3; %[m/s]
+        crit_high = 8; %[m/s]
+        crit_diff = 10; %[m/s /s]
+
+        time_low  = 2; %[s]
+        time_high = 0.2; %[s]
+        time_diff = 0.1; %[s]
+
+        count_low = 0;
+        count_high = 0;
+        count_diff = 0;
+
+        flag_low_fault  = 0;
+        flag_high_fault = 0;
+        flag_diff_fault = 0;
+
+        filter_freq = 0.1; %[Hz]
+
+        dt = 0.05; %[s]
+
+        low_pass_filter;
+
+    end
+
+    methods
+        function obj = residual_fault_detector(crit_low,crit_high,crit_diff,time_low,time_high,time_diff,filter_freq,dt)
+            obj.res = 0;
+            obj.res_filt = 0;
+            obj.res_diff = 0;
+            
+            obj.crit_low  = crit_low; %[m/s]
+            obj.crit_high = crit_high; %[m/s]
+            obj.crit_diff = crit_diff; %[m/s /s]
+    
+            obj.time_low  = time_low; %[s]
+            obj.time_high = time_high; %[s]
+            obj.time_diff = time_diff; %[s]
+    
+            obj.count_low  = 0;
+            obj.count_high = 0;
+            obj.count_diff = 0;
+    
+            obj.flag_low_fault  = 0;
+            obj.flag_high_fault = 0;
+            obj.flag_diff_fault = 0;
+    
+            obj.filter_freq = filter_freq; %[Hz]
+
+            obj.dt = dt;
+
+            [b,a] = butter(2,2*obj.filter_freq*dt,'low');
+            obj.low_pass_filter = filter_discrete(b,a,0,0);
+        end
+
+        function update_innov(obj,res)
+            last_res_filt = obj.res_filt;
+            obj.res = res;
+            obj.res_filt = obj.low_pass_filter.update_filter_discrete(res);
+            obj.res_diff = (obj.res_filt-last_res_filt)./obj.dt;
+        end
+
+        function check_thresholds(obj)
+
+            % High Threshold
+            if obj.res>obj.crit_high && ~obj.flag_high_fault
+                obj.flag_high_fault = 1;
+            elseif obj.res>obj.crit_high && obj.flag_high_fault
+                obj.count_high = obj.count_high+1;
+            else
+                obj.flag_high_fault = 0;
+                obj.count_high = 0;
+            end
+            
+            % Low Threshold
+            if obj.res_filt>obj.crit_low && ~obj.flag_low_fault
+                obj.flag_low_fault = 1;
+            elseif obj.res_filt>obj.crit_low && obj.flag_low_fault
+                obj.count_low = obj.count_low+1;
+            else
+                obj.flag_low_fault = 0;
+                obj.count_low = 0;
+            end
+            
+            % Diff Threshold
+            if obj.res_diff>obj.crit_diff && ~obj.flag_diff_fault
+                obj.flag_diff_fault = 1;
+            elseif obj.res_diff>obj.crit_diff && obj.flag_diff_fault
+                obj.count_diff = obj.count_diff+1;
+            else
+                obj.flag_diff_fault = 0;
+                obj.count_diff = 0;
+            end
+
+        end
+    end
+end
---
+++ plot_residual_fault_detector.m
@@ -0,0 +1,51 @@
+function plot_residual_fault_detector(t,res_fault_detector)
+
+res = res_fault_detector.res;
+count = res_fault_detector.count;
+flag = res_fault_detector.flag;
+dt = mean(diff(t));
+
+
+figure;
+ax1 = subplot(2,2,1);
+s1 = plot(t,res(1,:));
+hold on
+s2 = plot(t,res(2,:));
+s3 = yline(res_fault_detector.criterias.crit_low,'--','color',"#D95319");yline(-res_fault_detector.criterias.crit_low,'o--','color',"#D95319")
+s4 = yline(res_fault_detector.criterias.crit_high,'r--');yline(-res_fault_detector.criterias.crit_high,'r--')
+legend([s1,s2,s3,s4],'Raw Innovation','Low Passed Innovation','Low Threshold','High Threshold')
+xlabel('Time [s]')
+ylabel('Innovation [m/s]')
+axis([-inf inf -1.05*res_fault_detector.criterias.crit_high 1.05*res_fault_detector.criterias.crit_high])
+
+ax2 = subplot(2,2,2);
+s1 = plot(t,res(3,:));
+s2 = yline(res_fault_detector.criterias.crit_diff,'r--');yline(-res_fault_detector.criterias.crit_diff,'r--');
+legend([s1,s2],'Innovation Derivative','Threshold')
+xlabel('Time [s]')
+ylabel('Innovation Derivative [m/s /s]')
+axis([-inf inf -1.05*res_fault_detector.criterias.crit_diff 1.05*res_fault_detector.criterias.crit_diff])
+
+ax3 = subplot(2,2,3);
+s1 = plot(t,count(1,:)*dt);
+hold on
+s2 = plot(t,count(2,:)*dt);
+s3 = yline(res_fault_detector.criterias.time_low,'--','color',"#D95319");
+s4 = yline(res_fault_detector.criterias.time_high,'r--');
+legend([s1 s2 s3 s4],'Low Count','High Count','Limit Low','Limit High')
+xlabel('Time [s]')
+ylabel('Time for fault')
+axis([-inf inf 0 1.1*res_fault_detector.criterias.time_low])
+
+ax4 = subplot(2,2,4);
+s1 = plot(t,count(3,:)*dt);
+hold on
+s2 = yline(res_fault_detector.criterias.time_diff,'r--');
+legend([s1 s2],'Derivative Count','Limit')
+xlabel('Time [s]')
+ylabel('Time for fault')
+axis([-inf inf 0 1.1*res_fault_detector.criterias.time_diff])
+
+linkaxes([ax1,ax2,ax3,ax4],'x');
+
+end
